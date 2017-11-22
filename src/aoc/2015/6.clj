2d7a8d5d35c4fe0ed7ccacad11db54e4e4a08116
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-11-22 14:30:13 +0100

    2015 day 6

---
+++ src/aoc/2015/6.clj
@@ -0,0 +1,76 @@
+(ns aoc.2015.6
+  (:require [aoc.core :refer :all]))
+
+(def input (file-lines "2015/6.txt"))
+
+(def r #"^(turn (?:on|off)|toggle) (\d+),(\d+) through (\d+),(\d+)$")
+
+(def TURN_ON "turn on")
+(def TURN_OFF "turn off")
+(def TOGGLE "toggle")
+
+(def OFF 0)
+(def ON 1)
+
+(defn parse-instruction [s]
+  (let [parsed (rest (first (re-seq r s)))]
+    (conj (map read-string (rest parsed)) (first parsed))))
+
+(defn create-matrix [size]
+  (vec (take size (repeat (vec (take size (repeat OFF)))))))
+
+(defn state [grid y x]
+  (nth (nth grid y) x))
+
+(defn grid-action [ grid [ action y x] ]
+  (let [current (state grid y x)]
+  (condp = action
+    TURN_ON (assoc grid y (assoc (nth grid y) x ON))
+    TURN_OFF (assoc grid y (assoc (nth grid y) x OFF))
+    TOGGLE (assoc grid y (assoc (nth grid y) x (if (= ON current) OFF ON))))))
+
+(defn grid-action-part-two [ grid [ action y x] ]
+  (let [current (state grid y x)]
+  (condp = action
+    TURN_ON (assoc grid y (assoc (nth grid y) x (inc current)))
+    TURN_OFF (assoc grid y (assoc (nth grid y) x (if (> current 0) (dec current) current)))
+    TOGGLE (assoc grid y (assoc (nth grid y) x (+ current 2))))))
+
+(defn create-range-actions [ action y1 x1 y2 x2 ]
+  (for [y (range y1 (inc y2)) x (range x1 (inc x2))]
+    [action y x]))
+
+(defn pprint-grid [grid]
+  (println (for [r grid] (str r "\n"))))
+
+(defn range-action [ grid action y1 x1 y2 x2 ]
+  (reduce (fn [grid actions] (grid-action grid actions)) grid (create-range-actions action y1 x1 y2 x2)))
+
+(defn range-action-part-two [ grid action y1 x1 y2 x2 ]
+  (reduce (fn [grid actions] (grid-action-part-two grid actions)) grid (create-range-actions action y1 x1 y2 x2)))
+
+(defn parse-multiple-instruction [lines]
+  (map parse-instruction lines))
+
+(defn count-on [grid]
+  (count (filter #(= 1 %) (reduce concat grid))))
+
+(defn sum-brightness [grid]
+  (reduce + (reduce concat grid)))
+
+(defn part-one
+  ([] (part-one input 1000))
+  ([input grid-size] (let [ original-grid (create-matrix grid-size)
+                           actions (map parse-instruction input)
+                           new-grid (reduce (fn [grid [ action y1 x1 y2 x2 ]] (range-action grid action y1 x1 y2 x2)) original-grid actions)]
+                       (println "part one" (count-on new-grid))
+                       new-grid)))
+
+(defn part-two
+  ([] (part-two input 1000))
+  ([input grid-size] (let [ original-grid (create-matrix grid-size)
+                           actions (map parse-instruction input)
+                           new-grid (reduce (fn [grid [ action y1 x1 y2 x2 ]] (range-action-part-two grid action y1 x1 y2 x2)) original-grid actions)]
+                       (println "part two" (sum-brightness new-grid))
+                       new-grid)))
+
---
+++ test/aoc/2015/6_test.clj
@@ -0,0 +1,91 @@
+(ns aoc.2015.6_test
+  (:require [clojure.test :refer :all]
+            [aoc.core :refer :all]
+            [aoc.2015.6 :refer :all]))
+
+
+(deftest test-parse-instruction
+  (is (= [TURN_ON, 0, 0, 999, 999] (parse-instruction "turn on 0,0 through 999,999")))
+  (is (= [TOGGLE, 0, 0, 999, 0] (parse-instruction "toggle 0,0 through 999,0")))
+  (is (= [TURN_OFF, 499, 499, 500, 500] (parse-instruction "turn off 499,499 through 500,500"))))
+
+(deftest test-parse-multiple-instruction
+  (is (= [[TOGGLE 0 0 2 2] [TOGGLE 0 0 2 2]] (parse-multiple-instruction [ "toggle 0,0 through 2,2" "toggle 0,0 through 2,2" ]))))
+
+(deftest test-create-matrix
+  (is (= [[0 0 0]
+          [0 0 0]
+          [0 0 0]] (create-matrix 3))))
+
+(deftest test-state
+  (is (= ON (state [[0 1 0]] 0 1)))
+  (is (= OFF (state [[0 1 0]] 0 0)))
+  (is (= OFF (state [[0 1 0]] 0 2)))
+  (is (= ON (state [[0 0 0] [0 0 0] [1 0 0]] 2 0)))
+  (is (= ON (state [[0 0 0] [0 1 0] [0 0 0]] 1 1))))
+
+(deftest test-grid-action
+  (is (= [[0 0 0]
+          [0 1 0]
+          [0 0 0]] (grid-action (create-matrix 3) [TURN_ON 1 1])))
+  (is (= [[0 0 0]
+          [0 0 0]
+          [0 0 0]] (grid-action [[0 0 0] [0 1 0] [0 0 0]] [TOGGLE 1 1])))
+  (is (= [[0 0 0]
+          [0 0 0]
+          [0 0 0]] (grid-action [[0 0 0] [0 1 0] [0 0 0]] [TURN_OFF 1 1]))))
+
+(deftest test-grid-action-part-two
+  (is (= [[0 0 0]
+          [0 1 0]
+          [0 0 0]] (grid-action-part-two (create-matrix 3) [TURN_ON 1 1])))
+  (is (= [[0 0 0]
+          [0 3 0]
+          [0 0 0]] (grid-action-part-two [[0 0 0] [0 1 0] [0 0 0]] [TOGGLE 1 1])))
+  (is (= [[0 0 0]
+          [0 0 0]
+          [0 0 0]] (grid-action-part-two [[0 0 0] [0 1 0] [0 0 0]] [TURN_OFF 1 1]))))
+
+(deftest test-create-range-action
+  (is (= [[TURN_ON 0 0] [TURN_ON 0 1] [TURN_ON 0 2]
+          [TURN_ON 1 0] [TURN_ON 1 1] [TURN_ON 1 2]
+          [TURN_ON 2 0] [TURN_ON 2 1] [TURN_ON 2 2]] (create-range-actions TURN_ON 0 0 2 2))))
+
+(deftest test-range-action
+  (is (= [[0 0 0]
+          [1 1 1]
+          [0 0 0]] (range-action (create-matrix 3) TURN_ON 1 0 1 2)))
+  (is (= [[0 0 0]
+          [0 1 1]
+          [0 1 1]] (range-action (create-matrix 3) TURN_ON 1 1 2 2))))
+
+(deftest test-count-on
+  (is (= 4 (count-on [[0 0 0]
+                      [0 1 1]
+                      [0 1 1]]))))
+(deftest test-sum-brightness
+  (is (= 4 (sum-brightness [[0 0 0]
+                            [0 1 1]
+                            [0 1 1]])))
+  (is (= 5 (sum-brightness [[0 0 0]
+                            [0 2 1]
+                            [0 1 1]])))
+  (is (= 9 (sum-brightness [[1 1 1]
+                            [1 1 1]
+                            [1 1 1]]))))
+
+
+(deftest test-part-one
+  (is (= [[0 0 0]
+          [0 0 0]
+          [0 0 0]] (part-one [ "toggle 0,0 through 2,2" "toggle 0,0 through 2,2" ] 3)))
+  (is (= [[1 1 1]
+          [1 1 1]
+          [1 1 1]] (part-one [ "toggle 0,0 through 2,2" ] 3))))
+  ; (part-one))
+
+(deftest test-part-two
+  (is (= [[3 3 3]
+          [3 3 3]
+          [3 3 3]] (part-two [ "toggle 0,0 through 2,2" "toggle 0,0 through 2,2" "turn off 0,0 through 2,2" ] 3)))
+  (part-two))
