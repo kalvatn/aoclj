bf826873056968ec4f3feeb92dc2bee61ea1daf2
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-12-02 13:51:19 +0100

    cleanup 2015 11

--- src/aoc/2015/11.clj
+++ src/aoc/2015/11.clj
@@ -5,7 +5,7 @@
 
 (def input (first-line "2015/11.txt"))
 
-(def letter-straights (partition 3 1 (alphabet)))
+(def letter-straights (set (map #(apply str %) (partition 3 1 (alphabet)))))
 
 (defn increment-char [c]
   (if (= \z c) \a (char (inc (int c)))))
@@ -17,21 +17,32 @@
         (if (not= \a c) (apply str (flatten [(take n s) c r]))
           (recur (dec n) (conj r c)))))))
 
+(defn is-letter-straight? [s]
+  (contains? letter-straights s))
+
+(defn split-into-triples [s]
+  (map #(apply str %) (partition 3 1 s)))
+
 (defn has-letter-straight? [s]
-  (any? true? (pmap #(.contains s (apply str %)) letter-straights)))
+  ; (any? true? (pmap #(.contains s %) letter-straights))
+  (any? true? (map is-letter-straight? (split-into-triples s)))
+)
 
 (defn has-confusing? [s]
-  (any? true? (pmap #(.contains s %) ["i" "o" "l"])))
+  (boolean (re-matches #".*[iol].*" s)))
+
+(def not-confusing? (complement has-confusing?))
 
 (defn has-double-letter-pair? [s]
   (>= (count (filter #(>= (count %) 2) (set (partition-by identity s)))) 2))
 
 (defn is-valid? [s]
-  (and (not (has-confusing? s)) (has-letter-straight? s) (has-double-letter-pair? s) ))
+  (and (not-confusing? s) (has-double-letter-pair? s) (has-letter-straight? s)))
 
 (defn part-one [input]
   ; (increment-string (last (take-while #(not (is-valid? %)) (iterate increment-string input)))))
-  (first (drop-while #(not (is-valid? %)) (iterate increment-string input))))
+  (first (filter is-valid? (iterate increment-string input)))
+  )
 
 (defn part-two [input]
   (part-one (increment-string "hepxxyzz")))
