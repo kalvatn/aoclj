098addb914cc7346d6cb49a8c5f02987f4daffd4
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-12-04 17:26:00 +0100

    handle errors in runner

--- src/aoc/runner.clj
+++ src/aoc/runner.clj
@@ -12,11 +12,19 @@
 (defn- print-result-with-time [prefix function input]
   (time (print (format "%25s : %-15s" prefix (function input)))))
 
+(defn- print-error [year day error]
+   (println (format "error running aoc.%4d.%02d : %s" year day (.getMessage error))))
+
 (defn run-day
-  ([year day] (run-day year day (get-input year day)))
+  ([year day]
+   (try
+     (run-day year day (get-input year day))
+     (catch Exception e (print-error year day e))))
   ([year day input]
-   (let [modulename (format "aoc.%4d.%02d" year day)
-         pt1-fn (lookup year day "part-one")
-         pt2-fn (lookup year day "part-two")]
-     (print (with-out-str (print-result-with-time (str modulename "/part-one") pt1-fn input)))
-     (print (with-out-str (print-result-with-time (str modulename "/part-two") pt2-fn input))))))
+   (try
+     (let [modulename (format "aoc.%4d.%02d" year day)
+           pt1-fn (lookup year day "part-one")
+           pt2-fn (lookup year day "part-two")]
+       (print (with-out-str (print-result-with-time (str modulename "/part-one") pt1-fn input)))
+       (println (with-out-str (print-result-with-time (str modulename "/part-two") pt2-fn input))))
+     (catch Exception e (print-error year day e)))))
