351b4060c73c89433e9e013881ebd937359d3962
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-12-05 17:21:10 +0100

    2017-05 optimization
    - int-array is the winner

--- src/aoc/2017/05.clj
+++ src/aoc/2017/05.clj
@@ -10,28 +10,28 @@
 ; (def input (lines "2017/05_frabe.txt"))
 
 (defn parse-input [input]
-  (vec (map read-string input)))
+  (map read-string input))
 
-(defn v-fn-part-two ^long [^long v]
-  (if (>= v 3)
-    (dec v)
-    (inc v)))
-
-(defn count-steps [jumptable part-two?]
-  (let [size (count jumptable)]
-    (loop [vs (transient jumptable)
+; (defn count-steps [jumptable-vec v-fn] ; sending function as argument => 50ms slowdown
+(defn count-steps [jumptable-vec part-two?]
+  (let [jumptable (int-array jumptable-vec)
+        size (alength jumptable)]
+    (loop [vs jumptable
            i 0
            steps 0]
       (if (or (< i 0) (>= i size)) steps
-        (let [^long v (vs i)]
-          (recur
-            (assoc! vs i (if part-two? (v-fn-part-two v) (inc v)))
-            (+ v i)
-            (inc steps)))))))
+      (let [v (aget vs i)
+            ; v2 (int (v-fn v))]
+            v2 (if (and part-two? (>= v 3)) (dec v) (inc v))]
+        (recur (doto vs (aset i v2))
+               (+ i v)
+               (inc steps)))))))
 
 
 (defn part-one [input]
-  (count-steps (parse-input input) false))
+  ; (time (count-steps (parse-input input) (fn ^long [^long v] (inc v)))))
+  (time (count-steps (parse-input input) false)))
 
 (defn part-two [input]
-  (count-steps (parse-input input) true))
+  ; (time (count-steps (parse-input input) (fn ^long [^long v] (if (>= v 3) (dec v) (inc v))))))
+  (time (count-steps (parse-input input) true)))
