b3481a96cdfb31f0b74e6425938445032ee782bb
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-12-05 15:31:37 +0100

    2017-05 optimization

--- src/aoc/2017/05.clj
+++ src/aoc/2017/05.clj
@@ -7,33 +7,31 @@
 
 ; (def input (lines "2017/05_test.txt"))
 (def input (lines "2017/05.txt"))
+; (def input (lines "2017/05_frabe.txt"))
 
 (defn parse-input [input]
-  (transient (reduce into (map-indexed (fn [k v] { k v }) (map read-string input)))))
-(defn parse-input2 [input]
   (vec (map read-string input)))
-
-(defn jump [indexed-map v-fn]
-  (loop [value-map indexed-map i 0 steps 0]
-    (if (or (< i 0) (>= i (count value-map))) [ value-map steps ]
-      (let [v (get value-map i)
-            ^long new-index (+ v i)
-            ^long new-v (v-fn v)]
-        (recur (assoc! value-map i new-v) new-index (inc steps))))))
 
 (defn v-fn-part-two ^long [^long v]
   (if (>= v 3)
     (dec v)
     (inc v)))
 
+(defn count-steps [jumptable part-two?]
+  (let [size (count jumptable)]
+    (loop [vs (transient jumptable)
+           i 0
+           steps 0]
+      (if (or (< i 0) (>= i size)) steps
+        (let [^long v (vs i)]
+          (recur
+            (assoc! vs i (if part-two? (v-fn-part-two v) (inc v)))
+            (+ v i)
+            (inc steps)))))))
+
+
 (defn part-one [input]
-  (second (jump (parse-input input) inc)))
+  (count-steps (parse-input input) false))
 
 (defn part-two [input]
-  (let [initial-v (parse-input2 input)
-        size (count initial-v)]
-    (loop [v (transient initial-v) i (int 0) steps (int 0) ]
-      (if (and (>= i 0) (< i size))
-        (let [value (int (nth v i))] (recur (assoc! v i (int (v-fn-part-two value))) (int (+ i value)) (inc steps)))
-        steps))))
-
+  (count-steps (parse-input input) true))
