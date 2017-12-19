1c4f097083ff9a43a33512897925207a6ef88be5
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-12-19 02:28:33 +0100

    2017-10 part one

--- src/aoc/2017/10.clj
+++ src/aoc/2017/10.clj
@@ -1,9 +1,52 @@
 (ns aoc.2017.10
-  (:require [aoc.core.io :refer :all]))
+  (:require [aoc.core.io :refer :all]
+            [aoc.core.string :as s]))
 
-(def input (lines "2017/10.txt"))
+(def input (first-line "2017/10.txt"))
 
-(defn part-one [input]
-  )
+(defn parse-input [line]
+  (vec (map read-string (s/split line #","))))
+
+
+(defn select-range [coll current-index size]
+  (take size (drop current-index (cycle coll))))
+
+(defn reverse-range [coll current-index size]
+  (reverse (select-range coll current-index size)))
+
+(defn rotate [coll n]
+  (concat (drop n coll) (take n coll)))
+
+(defn knot [coll current-index size step-size]
+  (let [rot (rotate coll current-index)
+        pre (concat (reverse-range rot 0 size) (drop size rot))
+        fin (rotate pre (- (count coll) current-index))]
+    fin))
+
+(defn knot-hash [coll lengths]
+  (let [list-size (count coll)]
+    (loop [coll coll
+           i 0
+           pos 0
+           step 0]
+      (println coll i pos step)
+      (if (>= i (count lengths)) coll
+        (let [size (lengths i)]
+          (recur (knot coll pos size step)
+                 (inc i)
+                 (mod (+ pos size step) list-size)
+                 (inc step)))))))
+
+
+(defn part-one
+  ([input] (part-one input 256))
+  ([input list-size]
+   (let [initial (range 0 list-size)
+         lengths (parse-input input)
+         knotted (knot-hash initial lengths)
+         ]
+     (* (first knotted) (second knotted))
+     )
+   ))
 (defn part-two [input]
   )
--- test/aoc/2017/10_test.clj
+++ test/aoc/2017/10_test.clj
@@ -2,5 +2,29 @@
   (:require [clojure.test :refer :all]
             [aoc.2017.10 :refer :all]))
 
+(deftest test-parse-input
+  (is (= [3 4 1 5] (parse-input "3, 4, 1, 5"))))
+
+(deftest test-select-range
+  (is (= [0 1 2] (select-range [0 1 2 3 4] 0 3)))
+  (is (= [3 4 2 1] (select-range [2 1 0 3 4] 3 4)))
+  )
+
+(deftest test-reverse-range
+  (is (= [2 1 0] (reverse-range [0 1 2 3 4] 0 3)))
+  )
+
+(deftest test-knot
+  (is (= [2 1 0 3 4] (knot [0 1 2 3 4] 0 3 0)))
+  (is (= [4 3 0 1 2] (knot [2 1 0 3 4] 3 4 1)))
+  (is (= [3 4 2 1 0] (knot [4 3 0 1 2] 1 5 4)))
+  )
+
+(deftest test-rotate
+  (is (= [1 2 3 4 0] (rotate [0 1 2 3 4] 1)))
+  (is (= [2 3 4 0 1] (rotate [0 1 2 3 4] 2)))
+  (is (= [0 1 2 3 4] (rotate [0 1 2 3 4] 5)))
+  )
+
 (deftest test-part-one)
 (deftest test-part-two)
