d1fe2666cfb0c64b997635f423c957d2a771c880
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2018-01-03 00:56:26 +0100

    2017-15

--- test/aoc/2017/15_test.clj
+++ test/aoc/2017/15_test.clj
@@ -2,5 +2,9 @@
   (:require [clojure.test :refer :all]
             [aoc.2017.15 :refer :all]))
 
+(deftest test-parse-input
+  (is (= [19 201] (parse-input ["Generator A starts with 19"
+                                "Generator B starts with 201"]))))
+
 (deftest test-part-one)
 (deftest test-part-two)
