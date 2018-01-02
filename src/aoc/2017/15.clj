d6f4205de93911dee7b898339d9b242cd42d66f4
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2018-01-02 22:38:31 +0100

    2017-15 skeleton

---
+++ src/aoc/2017/15.clj
@@ -0,0 +1,9 @@
+(ns aoc.2017.15
+  (:require [aoc.core.io :refer :all]))
+
+(def input (lines "2017/15.txt"))
+
+(defn part-one [input]
+  )
+(defn part-two [input]
+  )
---
+++ test/aoc/2017/15_test.clj
@@ -0,0 +1,6 @@
+(ns aoc.2017.15-test
+  (:require [clojure.test :refer :all]
+            [aoc.2017.15 :refer :all]))
+
+(deftest test-part-one)
+(deftest test-part-two)
