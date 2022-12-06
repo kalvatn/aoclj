a50e29891c839201369d4c2551a472fa03c4ae6d
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2022-12-06 14:24:33 +0100

    lein ancient upgrade :check-clojure

--- project.clj
+++ project.clj
@@ -5,15 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Xmx1G"]
   :main aoc.runner
-  :global-vars {
-                *warn-on-reflection* true
-                }
-  :repl-options {
-                 :caught clj-stacktrace.repl/pst+}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [digest "1.4.6"]
-                 [org.clojure/math.combinatorics "0.1.4"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [potemkin "0.4.4"]
-                 [aysylu/loom "1.0.0"]
+  ; :global-vars { *warn-on-reflection* true }
+  :repl-options { :caught clj-stacktrace.repl/pst+ }
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [digest "1.4.10"]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/math.numeric-tower "0.0.5"]
+                 [potemkin "0.4.6"]
+                 [aysylu/loom "1.0.2"]
                  ])
